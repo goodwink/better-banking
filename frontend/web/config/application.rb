141e34173ed9c34513e5bc2d0fb7fb2f001becb7
Kyle S. Goodwin <goodwink@gmail.com>
2011-06-12 09:40:05 -0400

    Added haml

--- frontend/web/config/application.rb
+++ frontend/web/config/application.rb
@@ -43,5 +43,11 @@ module Web
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    config.generators do |g|
+      g.template_engine     :haml
+      g.test_framework      :rspec, :fixture => true, :views => false
+      g.orm                 :mongoid
+    end
   end
 end
